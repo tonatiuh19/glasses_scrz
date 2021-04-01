d4c3cac2bca46b1f1141050b86a848a533217697
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-01 16:01:41 -0600

    Release 2.0

--- quienessomos/index.php
+++ quienessomos/index.php
@@ -44,6 +44,108 @@ if ($conn->query($sql) === TRUE) {
 
     <!-- MAIN -->
 
+    <div class="site-section">
+    <div class="album py-5 bg-light">
+        <div class="container">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+          <div class="row">
+          <div id='collection-component-1614212293319'></div>
+      <script type="text/javascript">
+      /*<![CDATA[*/
+      (function () {
+        var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+        if (window.ShopifyBuy) {
+          if (window.ShopifyBuy.UI) {
+            ShopifyBuyInit();
+          } else {
+            loadScript();
+          }
+        } else {
+          loadScript();
+        }
+        function loadScript() {
+          var script = document.createElement('script');
+          script.async = true;
+          script.src = scriptURL;
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+          script.onload = ShopifyBuyInit;
+        }
+        function ShopifyBuyInit() {
+          var client = ShopifyBuy.buildClient({
+            domain: 'santa-cruz-eyewear.myshopify.com',
+            storefrontAccessToken: 'cfd6eae6f87f22474bb4dd773f5c9f68',
+          });
+          ShopifyBuy.UI.onReady(client).then(function (ui) {
+            ui.createComponent('collection', {
+              id: '248091312279',
+              node: document.getElementById('collection-component-1614212293319'),
+              moneyFormat: '%24%7B%7Bamount_with_comma_separator%7D%7D',
+              options: {
+        "product": {
+          "styles": {
+            "product": {
+              "@media (min-width: 601px)": {
+                "max-width": "calc(25% - 20px)",
+                "margin-left": "20px",
+                "margin-bottom": "50px",
+                "width": "calc(25% - 20px)"
+              }
+            }
+          },
+          "text": {
+            "button": "Soltar en carrito"
+          }
+        },
+        "productSet": {
+          "styles": {
+            "products": {
+              "@media (min-width: 601px)": {
+                "margin-left": "-20px"
+              }
+            }
+          }
+        },
+        "modalProduct": {
+          "contents": {
+            "img": false,
+            "imgWithCarousel": true,
+            "button": false,
+            "buttonWithQuantity": true
+          },
+          "styles": {
+            "product": {
+              "@media (min-width: 601px)": {
+                "max-width": "100%",
+                "margin-left": "0px",
+                "margin-bottom": "0px"
+              }
+            }
+          },
+          "text": {
+            "button": "Soltar en la bolsa"
+          }
+        },
+        "option": {},
+        "cart": {
+          "text": {
+            "total": "Subtotal",
+            "button": "Pagar"
+          }
+        },
+        "toggle": {}
+      },
+            });
+          });
+        }
+      })();
+      /*]]>*/
+      </script>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
 
     <div class="site-section">
       <div class="container">
--- tienda/index.php
+++ tienda/index.php
@@ -80,7 +80,7 @@ if ($conn->query($sql) === TRUE) {
       }
     },
     "text": {
-      "button": "Add to cart"
+      "button": "Soltar en carrito"
     }
   },
   "productSet": {
@@ -109,14 +109,14 @@ if ($conn->query($sql) === TRUE) {
       }
     },
     "text": {
-      "button": "Add to cart"
+      "button": "Soltar en carrito"
     }
   },
   "option": {},
   "cart": {
     "text": {
       "total": "Subtotal",
-      "button": "Checkout"
+      "button": "Pagar"
     }
   },
   "toggle": {}
@@ -128,6 +128,188 @@ if ($conn->query($sql) === TRUE) {
 /*]]>*/
 </script>
 
+
+<script type="text/javascript">
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'santa-cruz-eyewear.myshopify.com',
+      storefrontAccessToken: 'cfd6eae6f87f22474bb4dd773f5c9f68',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('collection', {
+        id: '262636994711',
+        node: document.getElementById('collection-component-1614212293319'),
+        moneyFormat: '%24%7B%7Bamount_with_comma_separator%7D%7D',
+        options: {
+  "product": {
+    "styles": {
+      "product": {
+        "@media (min-width: 601px)": {
+          "max-width": "calc(25% - 20px)",
+          "margin-left": "20px",
+          "margin-bottom": "50px",
+          "width": "calc(25% - 20px)"
+        }
+      }
+    },
+    "text": {
+      "button": "Soltar en carrito"
+    }
+  },
+  "productSet": {
+    "styles": {
+      "products": {
+        "@media (min-width: 601px)": {
+          "margin-left": "-20px"
+        }
+      }
+    }
+  },
+  "modalProduct": {
+    "contents": {
+      "img": false,
+      "imgWithCarousel": true,
+      "button": false,
+      "buttonWithQuantity": true
+    },
+    "styles": {
+      "product": {
+        "@media (min-width: 601px)": {
+          "max-width": "100%",
+          "margin-left": "0px",
+          "margin-bottom": "0px"
+        }
+      }
+    },
+    "text": {
+      "button": "Soltar en carrito"
+    }
+  },
+  "option": {},
+  "cart": {
+    "text": {
+      "total": "Subtotal",
+      "button": "Pagar"
+    }
+  },
+  "toggle": {}
+},
+      });
+    });
+  }
+})();
+/*]]>*/
+</script>
+
+<script type="text/javascript">
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'santa-cruz-eyewear.myshopify.com',
+      storefrontAccessToken: 'cfd6eae6f87f22474bb4dd773f5c9f68',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('collection', {
+        id: '262636961943',
+        node: document.getElementById('collection-component-1614212293319'),
+        moneyFormat: '%24%7B%7Bamount_with_comma_separator%7D%7D',
+        options: {
+  "product": {
+    "styles": {
+      "product": {
+        "@media (min-width: 601px)": {
+          "max-width": "calc(25% - 20px)",
+          "margin-left": "20px",
+          "margin-bottom": "50px",
+          "width": "calc(25% - 20px)"
+        }
+      }
+    },
+    "text": {
+      "button": "Soltar en carrito"
+    }
+  },
+  "productSet": {
+    "styles": {
+      "products": {
+        "@media (min-width: 601px)": {
+          "margin-left": "-20px"
+        }
+      }
+    }
+  },
+  "modalProduct": {
+    "contents": {
+      "img": false,
+      "imgWithCarousel": true,
+      "button": false,
+      "buttonWithQuantity": true
+    },
+    "styles": {
+      "product": {
+        "@media (min-width: 601px)": {
+          "max-width": "100%",
+          "margin-left": "0px",
+          "margin-bottom": "0px"
+        }
+      }
+    },
+    "text": {
+      "button": "Soltar en carrito"
+    }
+  },
+  "option": {},
+  "cart": {
+    "text": {
+      "total": "Subtotal",
+      "button": "Pagar"
+    }
+  },
+  "toggle": {}
+},
+      });
+    });
+  }
+})();
+/*]]>*/
+</script>
     </div>
   </div>
 </div>
